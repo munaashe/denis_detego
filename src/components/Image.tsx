61462ccd4b103d172f8cfb52ec6f83490b1c62a5
munaashe <denis.siduna@gmail.com>
2024-03-22 00:43:20 +0200

    render image from unsplash asyncronously

---
+++ src/components/Image.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+
+const Image = () => {
+    const [imageUrl, setImageUrl] = useState('');
+
+    useEffect(() => {
+        async function fetchImage() {
+            try {
+                const response = await fetch('https://source.unsplash.com/random/800x600/?mars');
+                if (response.ok) {
+                    setImageUrl(response.url);
+                } else {
+                    throw new Error('Failed to fetch image');
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchImage();
+    }, []);
+    return (
+        <div className='flex flex-col items-center justify-center'>
+            {imageUrl ? (
+                <div className="w-full">
+                    <img src={imageUrl} alt="Mars" className="w-full h-auto max-h-[600px]" />
+                </div>
+            ) : (
+                <ImageLoader />
+            )}
+        </div>
+    )
+}
+
+export default Image
+
+const ImageLoader = () => (
+    <div className="flex justify-center items-center h-screen bg-gray-200 w-full">
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+        <div className="border-4 border-gray-400 border-opacity-50 animate-pulse"></div>
+    </div>
+)
