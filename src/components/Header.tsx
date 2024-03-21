5166c0231a41134ec84f3d58c0e27c20bd875b41
munaashe <denis.siduna@gmail.com>
2024-03-22 00:41:01 +0200

    create sticky header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='sticky top-0 h-[56px] text-2xl bg-secondary text-tertiary1 flex items-center justify-center'>
+            We Going to Mars!
+        </div>
+    )
+}
+
+export default Header
